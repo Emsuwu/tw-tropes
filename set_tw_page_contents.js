82222d3802510c01be70c78694e9cf7f63469baa
Ems <169854277+Emsuwu@users.noreply.github.com>
2026-02-02 22:58:51 +0000

    Initial commit

---
+++ set_tw_page_contents.js
@@ -0,0 +1,63 @@
+const urlParams = new URLSearchParams(window.location.search)
+const titleID = urlParams.get("title")
+
+
+function readAndSetData(targetID) { 
+    console.log(`Loading contents of ${targetID}`)
+    Papa.parse("./Data/tw_database.csv", {
+        download: true,
+        header: true,
+        skipEmptyLines: true, 
+        complete: function(results) {
+            const data = results.data;
+            console.log(`Data: ${data}`)
+            const match = data.find(row => row.id === targetID);
+            console.log(`Match: ${match}`)
+            if (match) {
+                let rating = 10
+                let synopsis = 'Line 1 of synopsis! Some more.\nLine 2 of synopsis!\nEtc.'
+                synopsis = '<p>' + synopsis.replaceAll('\n', '</p><p>') + '</p>'
+                let provider_url = 'https://myanimelist.net/anime/226/Elfen_Lied'
+                let provider_name = 'MyAnimeList'
+
+                document.getElementById("title").textContent = match["title_en"];
+                document.getElementById("medium").innerHTML = `<strong>Medium: </strong>${match["medium"]}<br>`
+                document.getElementById("rating").innerHTML = `<strong>Rating: </strong>${10}<br>`
+                document.getElementById("thumbnail").innerHTML = `<img src="Images/Thumbnails/${match["thumbnail"]}" class="media-description-thumbnail">`
+                document.getElementById("synopsis").innerHTML = synopsis
+                document.getElementById("synopsis_attribution").innerHTML = `Synopsis provided by <a href="${provider_url}" target="_blank">${provider_name}</a>.`
+                
+                console.log(match["trigger_warnings"])
+                let formattedTWs = ''
+                if (match["trigger_warnings"] != ""){
+                    formattedTWs = '<li>' + match["trigger_warnings"].replaceAll(';;','</li><li>') + '</li>';
+                }
+                else{
+                    formattedTWs = '';
+                }
+                
+                document.getElementById("trigger_warnings").innerHTML = formattedTWs
+
+                let nrs = match["negative_representations"].split(';;');
+                let formattedNrs = []
+                let und = false
+                for (let nr of nrs) {
+                    let split = nr.split(':')
+                    formattedNrs.push(`<p><span class="text-content-subheading">${split[0]}</span><br>${split[1]}</p>`)
+                    if (split[0] == undefined || split[1] == undefined){
+                        und = true
+                        break;
+                    }
+                }
+                formattedNrs = formattedNrs.join('')
+                if (!und) document.getElementById("negative_representations").innerHTML = formattedNrs
+                else document.getElementById("negative_representations").innerHTML = ''
+
+            } else {
+                console.log("No match found for: " + targetID);
+            }
+        }
+    });
+}
+
+readAndSetData(titleID)
