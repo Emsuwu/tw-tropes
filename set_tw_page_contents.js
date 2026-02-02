e81dfd480e3753ea19de5b87925019c2059792c2
Ems <169854277+Emsuwu@users.noreply.github.com>
2026-02-02 23:08:09 +0000

    CSV Url

--- set_tw_page_contents.js
+++ set_tw_page_contents.js
@@ -3,8 +3,10 @@ const titleID = urlParams.get("title")
 
 
 function readAndSetData(targetID) { 
+    const csvURL = 'https://raw.githubusercontent.com/Emsuwu/tw-tropes/refs/heads/main/Data/260202-backup.csv'
+
     console.log(`Loading contents of ${targetID}`)
-    Papa.parse("./Data/tw_database.csv", {
+    Papa.parse("csvURL", {
         download: true,
         header: true,
         skipEmptyLines: true, 
